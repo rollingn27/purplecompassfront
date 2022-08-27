b0e40fe8f04521c8704c56e8764bee4a69a8a7e7
DESKTOP-IKU26JQ\330 <kjh2919@gmail.com>
2022-08-27 21:42:17 +0900

    error테스트

--- src/component/home/Body.tsx
+++ src/component/home/Body.tsx
@@ -26,3 +26,563 @@ const Body = () => {
 };
 
 export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
+import React, { useState, Suspense } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Kanban from '../Kanban/Kanban';
+
+import CreateUi from './CreateUi';
+import DashBoard from '../dashboard/DashBoard';
+
+const Body = () => {
+  const [checker, setChecker] = useState<string>('');
+
+  const navigator = () => {
+    if (checker === 'issue') {
+      return <Kanban />;
+    } else {
+      return <DashBoard />;
+    }
+  };
+  return (
+    <>
+      11111
+      <Sidebar setChecker={setChecker} />
+      {navigator()}
+      <CreateUi />
+    </>
+  );
+};
+
+export default Body;
